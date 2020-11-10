60c07a373ae8559e9564dc48bb5f2a56d16722fb
Jorrit Keijzer <jorrit@eli5.io>
2020-11-10 10:10:29 +0100

    Added get hash function WIP

--- crypto/main.go
+++ crypto/main.go
@@ -1,12 +1,24 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
 type Block struct {
-	hash     []byte
-	prevHash []byte
-	data     []byte
+	Hash     []byte
+	PrevHash []byte
+	Data     []byte
+}
+
+func (b *Block) GetHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash := hash[:]
+	fmt.Println(hash)
 }
 
 func main() {
 
 }
-
